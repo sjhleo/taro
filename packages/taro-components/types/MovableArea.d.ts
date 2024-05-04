0e73bc46eaa499c80df8961a3fe8b5578e86ba28
sjhleo <sjhleo@users.noreply.github.com>
2024-05-04 01:18:21 +0000

    refactor(types): sync components types

--- packages/taro-components/types/MovableArea.d.ts
+++ packages/taro-components/types/MovableArea.d.ts
@@ -7,10 +7,9 @@ interface MovableAreaProps extends StandardProps {
    */
   scaleArea?: boolean
 }
-
 /** movable-view 的可移动区域
  * @classification viewContainer
- * @supported weapp, alipay, swan, tt, qq, jd, h5, rn
+ * @supported weapp, alipay, swan, tt, qq, jd, h5, rn, harmony_hybrid
  * @example_react
  * ```tsx
  * class App extends Components {
--- packages/taro-components/types/Radio.d.ts
+++ packages/taro-components/types/Radio.d.ts
@@ -2,38 +2,37 @@ import { ComponentType } from 'react'
 import { StandardProps, CommonEventFunction, FormItemProps } from './common'
 interface RadioProps extends StandardProps {
   /** `<Radio/>` 标识。当该`<Radio/>` 选中时，`<RadioGroup/>`的 change 事件会携带`<Radio/>`的 value
-   * @supported weapp, alipay, swan, tt, qq, jd, rn
+   * @supported weapp, alipay, swan, tt, qq, jd, h5, rn
    */
   value?: string
-
   /** 当前是否选中
    * @default false
    * @supported weapp, alipay, swan, tt, qq, jd, h5, rn
    */
   checked?: boolean
-
   /** 是否禁用
    * @default false
    * @supported weapp, alipay, swan, tt, qq, jd, h5, rn
    */
   disabled?: boolean
-
   /** Radio 的颜色，同 css 的 color
    * @default "#09BB07"
    * @supported weapp, alipay, swan, tt, qq, jd, rn
    */
   color?: string
-
+  /**
+   * Radio 的名字
+   * @supported h5, harmony
+   */
+  name?: string
   /** 用于透传 `WebComponents` 上的属性到内部 H5 标签上
    * @supported h5
    */
   nativeProps?: Record<string, unknown>
-
   /** 无障碍访问，（属性）元素的额外描述
    * @supported qq
    */
   ariaLabel?: string
-
   /** <radio-group/> 中的选中项发生变化时触发 change 事件
    * @supported h5, rn
    */
@@ -41,10 +40,9 @@ interface RadioProps extends StandardProps {
     value?: string
   }>
 }
-
 /** 单选项目
  * @classification forms
- * @supported weapp, alipay, swan, tt, qq, h5, rn, harmony
+ * @supported weapp, alipay, swan, tt, qq, h5, rn, harmony, harmony_hybrid
  * @example_react
  * ```tsx
  * export default class PageRadio extends Component {
